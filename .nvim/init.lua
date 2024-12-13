1675f7ec4781ee08b20e4200ae4a7ec26328cc61
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-13 22:39:18 +0100

    nvim - set opts

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -3,3 +3,19 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+
+-- Set options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.scrolloff = 3
