70d22271a609fe6564c3a7fcead659953d876191
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-15 04:12:23 +0100

    nvim - show cursor line

--- .nvim/lua/config/settings.lua
+++ .nvim/lua/config/settings.lua
@@ -1,22 +1,23 @@
 -- Leader must be set before plugins are loaded.
+vim.g.have_nerd_font = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
-vim.g.have_nerd_font = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
 vim.opt.breakindent = true
-vim.opt.undofile = true
+vim.opt.cursorline = true
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 3
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.undofile = true
 
 -- Sync clipboard between os and nvim
 vim.schedule(function()
