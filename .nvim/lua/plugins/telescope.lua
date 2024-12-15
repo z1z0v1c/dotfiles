c6d290ebe850c58f8d05c823f15750bb8a89a382
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-15 21:17:01 +0100

    nvim - install telescope plugin

---
+++ .nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+   },
+}
