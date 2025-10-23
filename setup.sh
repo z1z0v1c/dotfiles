8f1551d225a77b6bbc975fab6958c26095a9c956
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-10-23 03:01:03 +0200

    [Bash] Include message in the git commit alias

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ CURRENT_DIR=$(pwd)
 
 # Symlink configuration files
 # Use `-f` to force overwrite existing files
-ln -sf $CURRENT_DIR/nvim ~/.config/nvim
+ln -sf $CURRENT_DIR/.bashrc ~/.bashrc
 ln -sf $CURRENT_DIR/.vimrc ~/.vimrc
+ln -sf $CURRENT_DIR/nvim ~/.config/nvim
 
 echo "Dotfiles symlinking complete!"
 
